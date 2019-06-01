e96a0b2636ec213866880f638b29486c55b59477
Treffiletti,Nicholas <nicholas.treffiletti@jpmchase.com>
2019-06-01 17:07:14 -0400

    new formula

--- xcodelinker.rb
+++ xcodelinker.rb
@@ -1,17 +1,34 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Xcodelinker < Formula
-  desc "xcodeliner script."
-  homepage "https://github.com/nicholasanthony/xcodelinker"
-  url "https://github.com/nicholasanthony/xcodelinker/raw/master/xcodelinker-1.0.tar.gz"
-  sha256 "8443118e257c4c109332ae58df932da99f3bd1291a67b8a8a0283f529bc4f48e"
-  version "1.0"
+  desc ""
+  homepage ""
+  url "https://github.com/NicholasAnthony/XCodeLinker/blob/master/xcodelinker-1.0.tar.gz"
+  sha256 "260f7548c518f7ab241a4904d8cddb6f564802f619563e00d1fc33ae130e2a55"
+  # depends_on "cmake" => :build
 
   def install
-    # install xcodelinker script, create symlink to script in /usr/local/bin
-    bin.install "xcodelinker"
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    system "make", "install" # if this fails, try separate make/make install steps
   end
 
   test do
-    # test script output
-    assert_equal %x('#{bin}/xcodelinker'), "Hello world!\n"
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test xcodelinker`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
   end
 end
